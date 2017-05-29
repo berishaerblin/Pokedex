38e22bdca1ecc19634f93e4ed4536f0aefbd2c06
berishaerblin <berishaerblin@gmail.com>
2017-05-29 03:47:52 +0200

    third commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,26 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Erblin Berisha on 5/29/17.
+//  Copyright © 2017 Erblin Berisha. All rights reserved.
+//
+
+import Foundation
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
